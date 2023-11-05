633239b40c46b63289de3de0ef06c81cab274574
Tanmay Wadkar <tanmaywadkar7@gmail.com>
2023-11-05 19:53:45 +0530

    Added JWT AuthEntry Point , JWT Auth Filter, JWT Helper and SecurityConfig

--- src/main/java/com/jwtauthentication/token/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/jwtauthentication/token/security/JwtAuthenticationEntryPoint.java
@@ -5,12 +5,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class JwtAuthenticationEntryPoint {
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 	
 	public void commence (HttpServletRequest request, HttpServletResponse response,AuthenticationException authException) throws IOException,ServletException {
     
