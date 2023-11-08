3f21a6bf887cfc9ff694759ecf2230526c8b8f27
Tanmay Wadkar <tanmaywadkar7@gmail.com>
2023-11-08 10:36:29 +0530

    Added Request and Response Models

---
+++ src/main/java/com/jwtauthentication/token/models/JwtRequest.java
@@ -0,0 +1,17 @@
+package com.jwtauthentication.token.models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class JwtRequest {
+	
+	private String email;
+	
+	private String password;
+
+}
